6d0791904ae5079b00f58c45f154b5c94b484560
artusvranken <artusvranken@gmail.com>
2017-12-29 18:27:32 +0100

    Added images.

--- js/tsama.js
+++ js/tsama.js
@@ -45,7 +45,7 @@ function loadTsamagotchi()
             yourTsamagotchi._health = yourTsamagotchiDO._health;
             yourTsamagotchi._satiety = yourTsamagotchiDO._satiety;
             yourTsamagotchi._happiness = yourTsamagotchiDO._happiness;
-            yourTsamagotchi.isAlive = yourTsamagotchiDO._isAlive;
+            yourTsamagotchi._isAlive = yourTsamagotchiDO._isAlive;
             yourTsamagotchi._foodLimit = yourTsamagotchi._foodLimit;
             
             return yourTsamagotchi;
@@ -67,7 +67,17 @@ var app = new Vue({
   el: '#tsama-app-container',
   data: {
     tsama: loadTsamagotchi(),
-    maxBarWidth: 260
+    maxBarWidth: 260,
+    images : {
+        "happy1": "img/happy-1.png",
+        "happy2": "img/happy-2.png",
+        "sad1": "img/sad-1.png",
+        "sad2": "img/sad-2.png",
+        "mad1": "img/mad-1.png",
+        "mad2": "img/mad-2.png",
+        "dead": "img/dead.png"
+    },
+    imgToggle: false
   },
   methods: {
       newTsama : function ()
@@ -91,11 +101,30 @@ var app = new Vue({
           let toReturn = this.tsama.happiness / 100 * this.maxBarWidth;
           if (toReturn > this.maxBarWidth) return this.maxBarWidth;
           return toReturn;
+      },
+      currentImage : function()
+      {
+          this.imgToggle = !this.imgeToggle;
+          
+          if (this.tsama.isAlive)
+          {
+              if (this.tsama.health < 50) 
+              {
+                  return this.images["sad1"];
+              }
+              if (this.tsama.happiness < 50) return this.images["mad1"];
+              
+              return this.images["happy1"];
+          }
+          return this.images["dead"];
       }
   }
 });
 
+
+
 app.tsama.mature();
+
 setInterval(function() {
     app.tsama.mature();
     saveTsamagotchi(app.tsama);
